b242b9d0b845f9ff6e04c868993b4b26b1f5429f
purushotham_singh <kpurushothamsingh@gmail.com>
2022-08-19 11:51:12 +0530

    linq_concepts

---
+++ LinqConcept/LinqConcept/LinqConcepts2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqConcept
+{
+    internal class LinqConcepts2
+    {
+        public static List<LinqConcepts2> data2 = new List<LinqConcepts2>();
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Cost { get; set; }
+
+        public static List<LinqConcepts2> getalldata()
+        {
+            data2.Add(new LinqConcepts2(1, "pen", 30));
+            data2.Add(new LinqConcepts2(2, "ball", 45));
+
+            return data2;
+
+        }
+
+        public LinqConcepts2(int id, string name, int cost)
+        {
+            Id = id;
+            Name = name;
+            Cost = cost;
+        }
+
+
+        public override string ToString()
+        {
+            return String.Format(Id + " " + Name + " " + Cost);
+        }
+    }
+
+
+
+}
---
+++ LinqConcept/LinqConcept/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace LinqConcept
+{
+    internal class Program
+    {
+        public static List<LinqConcepts2> data = LinqConcepts2.getalldata();
+        static void Main()
+        {
+
+             
+            foreach(LinqConcepts2 item in data)
+            {
+                Console.WriteLine(item.ToString());
+                //ehllo
+            }
+
+            var filter = data.Where(d => d.Id == 2).Select(d => d);
+            foreach(LinqConcepts2 item in filter)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+
+        //static void Main()
+        //{
+        //    List<int> nums = new List<int>();
+
+        //    nums.Add(40);
+        //    nums.Add(30);
+        //    nums.Add(60);
+        //    nums.Add(89);
+        //    nums.Add(12);
+
+        //    var result = (from i in nums where i > 40 select i).ToList();
+        //    foreach (var item in result)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //    Dictionary<int, string> fruits = new Dictionary<int, string>();
+
+        //    fruits.Add(1, "apple");
+        //    fruits.Add(2, "banana");
+        //    fruits.Add(3, "carrot");
+
+        //    var result2 = (from i in fruits where i.Value.StartsWith("a") select i).ToList();
+        //    foreach (var item in result2)
+        //    {
+        //        Console.WriteLine(item);
+        //    }
+
+        //    var res = fruits.Where(a => a.Value.StartsWith("c")).Select(a => a);
+        //    foreach(var item in res)
+        //    {
+        //        Console.WriteLine(item.Value);
+        //    }
+
+
+        //}
+    }
+}
