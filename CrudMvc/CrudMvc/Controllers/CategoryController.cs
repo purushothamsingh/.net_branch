2c1803937443d980b54b22e5b6e6aad860591e3e
purushothamsingh <104088683+purushothamsingh@users.noreply.github.com>
2022-09-08 23:45:38 +0530

    added server side validations

--- CrudMvc/CrudMvc/Controllers/CategoryController.cs
+++ CrudMvc/CrudMvc/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using CrudMvc.Data;
 using CrudMvc.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 namespace CrudMvc.Controllers
 {
@@ -11,8 +12,6 @@ namespace CrudMvc.Controllers
 
         public IActionResult Index()
         {
-
-            
             using (var db = new ApplicationDbContext())
             {
                var ob =  db.Categories.ToList();
@@ -20,7 +19,31 @@ namespace CrudMvc.Controllers
             }
 
         }
-           
+
+        public IActionResult Create()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(Category obj)
+        {
+           using(var db = new ApplicationDbContext())
+            {
+
+                if (ModelState.IsValid)
+                {
+                    db.Categories.Add(obj);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Category");  // action - controller 
+                }
+              return View(obj);
+            }
+        }
+
 
 
 
