c902320680438383e3651dc4bd95c18c70716907
purushothamsingh <104088683+purushothamsingh@users.noreply.github.com>
2022-09-08 12:09:47 +0530

    added_migration_mvc

---
+++ CrudMvc/CrudMvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CrudMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudMvc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() { }
+
+
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // optionsBuilder.UseSqlServer(@"Server=(.);Database=ManagementDB;Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer("server=.;database=Bulky;trusted_connection=true;");
+          //  optionsBuilder.UseSqlServer(connectionString: "MyConn");
+        }
+        DbSet<Category> Categories {get; set; }
+    }
+}
